1593e48c01ab635b8a2bf377db77039711a44260
MarilynDafa <MarilynDafa@users.noreply.github.com>
2020-07-23 23:02:51 +0800

    Update ijpre.h

--- code/headers/ijpre.h
+++ code/headers/ijpre.h
@@ -23,8 +23,8 @@
 #define __ijpre_h_
 
 #define IJJS_VERSION_MAJOR 1
-#define IJJS_VERSION_MINOR 4
-#define IJJS_VERSION_PATCH 1
+#define IJJS_VERSION_MINOR 5
+#define IJJS_VERSION_PATCH 0
 #define IJJS_VERSION_SUFFIX ""
 #define QJS_VERSION_STR "20200705"
 
